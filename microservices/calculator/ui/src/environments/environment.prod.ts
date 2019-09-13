e65fd141ad2b43f491d685947c7cf6cc88572134
abalasu1 <abalasu1@in.ibm.com>
2019-09-13 11:49:37 +0530

    remove istio references from calculator microservices

--- microservices/calculator/ui/src/environments/environment.prod.ts
+++ microservices/calculator/ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  processorUrl: 'http://processor-calculator.apps.bank01.com/processor/calculate?verbosity=1'
-};
+    production: true,
+    processorUrl: 'http://processor.apps.9.30.140.180.xip.io/processor/calculate?verbosity=1'
+  };
--- microservices/calculator/ui/src/environments/environment.ts
+++ microservices/calculator/ui/src/environments/environment.ts
@@ -1,9 +1,10 @@
+
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  processorUrl: 'http://processor-calculator.apps.bank01.com/processor/calculate?verbosity=1',
-};
+    production: false,
+    processorUrl: 'http://processor.apps.9.30.140.180.xip.io/processor/calculate?verbosity=1',
+  };
